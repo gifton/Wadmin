8e9cd2487f01b94250c1c6969f19979dbacb68ab
gifton <giftono@gmail.com>
2019-07-29 23:58:34 -0700

    initial commit

---
+++ Wadmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wadmin
+//
+//  Created by Dev on 7/29/19.
+//  Copyright © 2019 Wesaturate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
